07c3d44ac9e45e37bd4915a8c13d8950835f7fa0
Aditya Singh <adisingh925@gmail.com>
2024-05-28 21:36:25 +0530

    added mocks

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,17 +1,46 @@
-from brownie import accounts, network, Lottery, config
+from brownie import network, Lottery, config
 import time
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, fund_with_link, get_contract
 
-def deploy():
+def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy(config["networks"][network.show_active()]["eth_usd_price_feed"],
-                            config["networks"][network.show_active()]["vrf_coordinator"],
-                            config["networks"][network.show_active()]["link_token"],
-                            config["networks"][network.show_active()]["fee"],
-                            config["networks"][network.show_active()]["key_hash"] ,
-                            {"from":account}
-                            )
+    Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["key_hash"] ,
+        {"from":account}
+    )
     time.sleep(1)
+    print("Lottery Deployed!")
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    start_lottery_txn = lottery.startLottery({"from":account})
+    start_lottery_txn.wait(1)
+    print("The lottery is started!")
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    lottery_enter_txn = lottery.enter({"from":account, "value" : value})
+    lottery_enter_txn.wait(1)
+    print("You entered the lottery!")
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    txn = fund_with_link(lottery.address)
+    txn.wait(1)
+    lottery_end_txn = lottery.endLottery({"from":account})
+    lottery_end_txn.wait(1)
+    time.sleep(60)
+    print("Lottery is ended and the winner is -> " + lottery.recentWinner)
 
 def main():
-    deploy()
+    deploy_lottery()
+    start_lottery()
+    enter_lottery()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,46 @@
-from brownie import network, accounts, config
+from brownie import network, accounts, config, interface, MockV3Aggregator, LinkToken, VRFCoordinatorMock, Contract
+import time
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
 
 def get_account():
     if(network.show_active() in "sepolia"):
         return accounts.add(config["wallets"]["from_key"])
     elif(network.show_active() in "mainnet-fork"):
         return accounts[0]
+    
+def fund_with_link(contract_address):
+    account = get_account()
+    amount = 250000000000000000
+    link_token = config["networks"][network.show_active()]["link_token"]
+    link_token_contract = interface.LinkTokenContract(link_token)
+    txn = link_token_contract.transfer(contract_address, amount, {"from":account})
+    txn.wait(1)
+    print("Contract funded with link token!")
+    return txn
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, initial_value, {"from":account})
+    time.sleep(1)
+    link_token = LinkToken.deploy({"from":account})
+    time.sleep(1)
+    VRFCoordinatorMock.deploy(link_token.address, {"from":account})
+
+get_contract_from_name = {"eth_usd_price_feed" : MockV3Aggregator, "vrf_coordinator" : VRFCoordinatorMock, "link_token" : LinkToken}
+
+def get_contract(contract_name):
+    contract_type = get_contract_from_name[contract_name]
+    if network.show_active() in "mainnet-fork":
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_Active()][contract_name]
+        contract = Contract.from_abi(contract_type._name, contract_address, contract_type.abi)
+
+    return contract
+
+    
